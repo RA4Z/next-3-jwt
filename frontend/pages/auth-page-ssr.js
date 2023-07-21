8d5d474a0d78a6a15fb27baf3b7090d095bf062c
RA4Z <rraz639@gmail.com>
2023-07-21 12:26:34 -0300

    Aplicando cookies e Storage no projeto

--- frontend/pages/auth-page-ssr.js
+++ frontend/pages/auth-page-ssr.js
@@ -1,11 +1,27 @@
+import { tokenService } from "../src/services/auth/tokenService"
+import nookies from 'nookies'
+
 export default function AuthPageSSR(props) {
+    console.log(tokenService.get())
     return (
         <div>
             <h1>
                 Auth Page Server Side Render
-            </h1><pre>
+            </h1>
+            <pre>
                 {JSON.stringify(props, null, 2)}
             </pre>
         </div>
     )
+}
+
+export async function getServerSideProps(ctx) {
+    console.log()
+    const cookies = nookies.get(ctx)
+    console.log('cookies', cookies)
+    return {
+        props: {
+            token: tokenService.get(ctx)
+        },
+    }
 }
