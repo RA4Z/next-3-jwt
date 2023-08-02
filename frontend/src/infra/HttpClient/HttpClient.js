5d310be5a0c19b1ab50480e597ee35dd00e55471
RA4Z <rraz639@gmail.com>
2023-08-02 12:19:03 -0300

    Trabalhando com atualização dos Tokens

--- frontend/src/infra/HttpClient/HttpClient.js
+++ frontend/src/infra/HttpClient/HttpClient.js
@@ -1,4 +1,7 @@
 // ARQUITETURA HEXAGONAL
+
+import { tokenService } from "../../services/auth/tokenService";
+
 // PORTS & ADAPTERS
 export async function HttpClient(fetchUrl, fetchOptions) {
     const options = {
@@ -17,5 +20,30 @@ export async function HttpClient(fetchUrl, fetchOptions) {
                 statusTexto: respostaDoServidor.statusText,
                 body: await respostaDoServidor.json(),
             }
-        });
+        })
+        .then(async (response) => {
+            if (!fetchOptions.refresh) return response;
+            if(fetchOptions.refresh != 401) return response;
+
+            console.log('Middleware: Rodar código para atualizar o token')
+            // Try to update tokens
+            const refreshResponse = await HttpClient('http://localhost:3000/api/refresh', {
+                method: 'GET'
+            })
+            const newAccessToken = refreshResponse.body.data.access_token;
+            const newRefreshToken = refreshResponse.body.data.refresh_token;
+
+            // Storage the Tokens
+            tokenService.save(newAccessToken)
+
+            // Try the execute the earlier request
+            const retryResponse = await HttpClient(fetchUrl, {
+                ...options,
+                refresh: false,
+                headers: {
+                    'Authorization': `Bearer ${newAccessToken}`
+                }
+            })
+            return retryResponse;
+        })
 }
