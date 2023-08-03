fef3eda4178e4683ce7d3120e361a841e9ecbac1
RA4Z <rraz639@gmail.com>
2023-08-03 12:22:07 -0300

    Ciclo de atualização dos Tokens

--- frontend/src/infra/HttpClient/HttpClient.js
+++ frontend/src/infra/HttpClient/HttpClient.js
@@ -1,6 +1,7 @@
 // ARQUITETURA HEXAGONAL
 
 import { tokenService } from "../../services/auth/tokenService";
+import nookies from 'nookies';
 
 // PORTS & ADAPTERS
 export async function HttpClient(fetchUrl, fetchOptions) {
@@ -23,27 +24,46 @@ export async function HttpClient(fetchUrl, fetchOptions) {
         })
         .then(async (response) => {
             if (!fetchOptions.refresh) return response;
-            if(fetchOptions.refresh != 401) return response;
+            if (fetchOptions.refresh != 401) return response;
 
+            const isServer = Boolean(fetchOptions?.ctx);
+            const currentRefreshToken = fetchOptions?.ctx?.req?.cookies['REFRESH_TOKEN_NAME'];
             console.log('Middleware: Rodar código para atualizar o token')
-            // Try to update tokens
-            const refreshResponse = await HttpClient('http://localhost:3000/api/refresh', {
-                method: 'GET'
-            })
-            const newAccessToken = refreshResponse.body.data.access_token;
-            const newRefreshToken = refreshResponse.body.data.refresh_token;
 
-            // Storage the Tokens
-            tokenService.save(newAccessToken)
 
-            // Try the execute the earlier request
-            const retryResponse = await HttpClient(fetchUrl, {
-                ...options,
-                refresh: false,
-                headers: {
-                    'Authorization': `Bearer ${newAccessToken}`
+            try {
+                // Try to update tokens
+                const refreshResponse = await HttpClient('http://localhost:3000/api/refresh', {
+                    method: isServer ? 'PUT' : 'GET',
+                    body: isServer ? { currentRefreshToken } : undefined
+                })
+                const newAccessToken = refreshResponse.body.data.access_token;
+                const newRefreshToken = refreshResponse.body.data.refresh_token;
+
+                if (isServer) {
+                    nookies.set(fetchOptions.ctx, 'REFRESH_TOKEN_NAME', newRefreshToken, {
+                        httpOnly: true,
+                        sameSite: 'lax',
+                        path: '/'
+                    })
                 }
-            })
-            return retryResponse;
+
+                // Storage the Tokens
+                tokenService.save(newAccessToken)
+
+                // Try the execute the earlier request
+                const retryResponse = await HttpClient(fetchUrl, {
+                    ...options,
+                    refresh: false,
+                    headers: {
+                        'Authorization': `Bearer ${newAccessToken}`
+                    }
+                })
+                console.log('retryResponse', retryResponse)
+                return retryResponse;
+            } catch (err) {
+                console.error(err)
+                return response;
+            }
         })
 }
